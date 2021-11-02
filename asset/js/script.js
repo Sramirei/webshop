ec64908e7ec3780e5febb21cc4d71b140e1d52fe
Sebastian Ramirez <51139115+Sramirei@users.noreply.github.com>
2021-11-01 23:00:40 -0500

    Add files via upload

---
+++ asset/js/script.js
@@ -0,0 +1,46 @@
+// Comparativo
+
+document.addEventListener('DOMContentLoaded', function() {
+  var parent = document.querySelector('.splitview'),
+      topPanel = parent.querySelector('.top'),
+      handle = parent.querySelector('.handle'),
+      skewHack = 0,
+      delta = 0;
+
+  // If the parent has .skewed class, set the skewHack var.
+  if (parent.className.indexOf('skewed') != -1) {
+      skewHack = 1000;
+  }
+
+  parent.addEventListener('mousemove', function(event) {
+      // Get the delta between the mouse position and center point.
+      delta = (event.clientX - window.innerWidth / 2) * 0.5;
+
+      // Move the handle.
+      handle.style.left = event.clientX + delta + 'px';
+
+      // Adjust the top panel width.
+      topPanel.style.width = event.clientX + skewHack + delta + 'px';
+  });
+});
+
+
+// Modal
+
+$(document).ready(function () {
+
+    function showPopup(){
+        $('.pop-up').addClass('show');
+        $('.pop-up-wrap').addClass('show');
+    }
+
+    $("#close").click(function(){
+        $('.pop-up').removeClass('show');
+        $('.pop-up-wrap').removeClass('show');
+    });
+
+    $(".btn-abrir").click(showPopup);
+
+    setTimeout(showPopup, 2000);
+
+});
